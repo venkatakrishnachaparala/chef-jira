da012fe6f7b6614b74a75d8622e68fb099f1df67
venkatakrishnachaparala <57476029+venkatakrishnachaparala@users.noreply.github.com>
2019-11-13 15:14:37 +0530

    Update apache2.rb

--- recipes/apache2.rb
+++ recipes/apache2.rb
@@ -5,11 +5,11 @@
 
 # TODO: Remove this work-around once a fix makes it into apache2 cookbook.
 # See: https://github.com/svanzoest-cookbooks/apache2/issues/398
-log 'forcing apache restart' do
-  notifies :restart, 'service[apache2]'
-  only_if { node['platform'] == 'ubuntu' && node['platform_version'].to_f == 12.04 }
-end
+#log 'forcing apache restart' do
+ # notifies :restart, 'service[apache2]'
+  #only_if { node['platform'] == 'ubuntu' && node['platform_version'].to_f == 12.04 }
+#end
 
-web_app node['jira']['apache2']['virtual_host_alias'] do
-  cookbook node['jira']['apache2']['template_cookbook']
-end
+#web_app node['jira']['apache2']['virtual_host_alias'] do
+ # cookbook node['jira']['apache2']['template_cookbook']
+#end
