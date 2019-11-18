887c803ecd96e803cf1994a3ccc042214ab0ca89
venkatakrishnachaparala <57476029+venkatakrishnachaparala@users.noreply.github.com>
2019-11-18 18:42:33 +0530

    Update apache2.rb

--- recipes/apache2.rb
+++ recipes/apache2.rb
@@ -17,4 +17,5 @@ end
 apache2_default_site node['jira']['apache2']['virtual_host_alias'] do
   default_site_name 'jira'
   template_cookbook node['jira']['apache2']['template_cookbook']
+  template_source 'jira/jira.conf.erb'
 end
